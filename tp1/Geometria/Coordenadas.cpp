1a0bfb7af31ee3c974a765fe31efba2c44037727
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-03-22 23:31:38 +0000

    Se sube el proyecto inicial. Ya esta dibujando lineas con el algoritmo DDA. (correr para ver ejemplo)
    Notar esquema de clases para continuar el proyecto de la misma forma
    
    a ganarrrr nico!! ;)

---
+++ tp1/Geometria/Coordenadas.cpp
@@ -0,0 +1,24 @@
+#include "Coordenadas.h"
+
+Coordenadas::Coordenadas()
+{
+    this->x = 0;
+    this->y = 0;
+}
+
+Coordenadas::Coordenadas(unsigned int x, unsigned int y){
+    this->x = x;
+    this->y = y;
+}
+
+
+Coordenadas::~Coordenadas()
+{
+    //dtor
+}
+
+double Coordenadas::distancia (Coordenadas* hasta){
+    unsigned int deltaX = hasta->getX() - this->x;
+    unsigned int deltaY = hasta->getY() - this->y;
+    return sqrt(deltaX*deltaX + deltaY*deltaY);
+}
---
+++ tp1/Geometria/FiguraGeometrica.h
@@ -0,0 +1,21 @@
+#ifndef FIGURAGEOMETRICA_H
+#define FIGURAGEOMETRICA_H
+
+#include "Coordenadas.h"
+
+class FiguraGeometrica
+{
+    public:
+        FiguraGeometrica();
+        virtual ~FiguraGeometrica();
+
+        /**
+         * Metodo abstracto Dibujar.
+         * Dibuja la figura en la pantalla de trabajo actual.
+         */
+        virtual void dibujar() = 0;
+    protected:
+    private:
+};
+
+#endif // FIGURAGEOMETRICA_H
