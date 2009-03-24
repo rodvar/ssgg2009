3628bef65f237dd6741c25a7c0219a06b4015c93
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-03-24 21:26:10 +0000

    +Se sube clase Circunferencia

---
+++ trunk/tp1/Geometria/Circunferencia.cpp
@@ -0,0 +1,42 @@
+#include "Circunferencia.h"
+
+Circunferencia::Circunferencia(float radio, Coordenadas* centro)
+{
+    this->radio = radio;
+    this->centro = centro;
+}
+
+Circunferencia::~Circunferencia()
+{
+    delete this->centro;
+}
+
+void Circunferencia::dibujar(){
+    this->dibujarBresenham();
+    //this->dibujarDDA();
+}
+
+void Circunferencia::dibujarBresenham(){
+    int x, y, e;
+    unsigned int xCentro = this->centro->getX();
+    unsigned int yCentro = this->centro->getY();
+    x = floor(this->radio);
+    y = 0;
+    e = 0;
+    while ( y <= x) {
+        glVertex2i(xCentro + x, yCentro + y ); glVertex2i(xCentro + y, yCentro + x );
+        glVertex2i(xCentro - x, yCentro + y ); glVertex2i(xCentro - y, yCentro + x );
+        glVertex2i(xCentro + x, yCentro - y ); glVertex2i(xCentro + y, yCentro - x );
+        glVertex2i(xCentro - x, yCentro - y ); glVertex2i(xCentro - y, yCentro - x );
+        e = e + 2*y + 1;
+        y++;
+        if ( 2*e > ( 2*x - 1)) {
+            x--;
+            e = e - 2*x + 1;
+        }
+    }
+}
+
+void Circunferencia::dibujarDDA(){
+    //TODO: Haceme nicaaaa ;)
+}
