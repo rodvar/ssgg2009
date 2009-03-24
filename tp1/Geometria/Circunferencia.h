7021a9083eaab0424da2d622870f37843571f636
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-03-24 21:26:24 +0000

    +Se sube clase Circunferencia

---
+++ tp1/Geometria/Circunferencia.h
@@ -0,0 +1,42 @@
+#ifndef CIRCUNFERENCIA_H
+#define CIRCUNFERENCIA_H
+
+#include "FiguraGeometrica.h"
+
+/**
+ * Clase que representa una circunferencia en el plano 2D
+**/
+
+class Circunferencia : public FiguraGeometrica
+{
+public:
+    /**
+     * @param radio Es el radio de la circunferencia
+     * @param centro Las coordenadas del centro de la misma
+     * @note Se hace responsable por la destruccion del centro
+     */
+    Circunferencia(float radio, Coordenadas* centro);
+
+    virtual ~Circunferencia();
+    float getRadio() { return radio; }
+    unsigned int getXCentro(){ return this->centro->getX(); }
+    unsigned int getYCentro(){ return this->centro->getY(); }
+
+    /**
+     * Dibuja la circunferencia en la pantalla actual
+     */
+    void dibujar();
+
+protected:
+private:
+    // Dibuja por el algoritmo DDA
+    void dibujarDDA();
+    // Dibuja por el algoritmo Bresenham
+    void dibujarBresenham();
+
+    // Variables miembro
+    float radio;
+    Coordenadas* centro;
+};
+
+#endif // CIRCUNFERENCIA_H
