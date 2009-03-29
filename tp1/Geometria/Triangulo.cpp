449832b5044bc548f5de52d9fb2c1a8502623e88
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-03-29 19:01:05 +0000

    +Se crea la clase Triangulo (ya se puede dibujar, correr el proyecto para verlo) + Se agrega funcionalidad a Coordenadas, corrigiendo bug en calculo de distancia + Se implementan metodos de Segmento (pendiente y longitud)

---
+++ tp1/Geometria/Triangulo.cpp
@@ -0,0 +1,28 @@
+#include "Triangulo.h"
+#include "Segmento.h"
+
+Triangulo::Triangulo(Coordenadas* verticeA, Coordenadas* verticeB, Coordenadas* verticeC)
+{
+    this->verticeA = verticeA;
+    this->verticeB = verticeB;
+    this->verticeC = verticeC;
+}
+
+Triangulo::~Triangulo()
+{
+    delete this->verticeA;
+    delete this->verticeB;
+    delete this->verticeC;
+}
+
+void Triangulo::dibujar(){
+    Segmento* segmentoAB = new Segmento(this->verticeA->copia(), this->verticeB->copia());
+    Segmento* segmentoBC = new Segmento(this->verticeB->copia(), this->verticeC->copia());
+    Segmento* segmentoCA = new Segmento(this->verticeC->copia(), this->verticeA->copia());
+    segmentoAB->dibujar();
+    segmentoBC->dibujar();
+    segmentoCA->dibujar();
+    delete segmentoAB;
+    delete segmentoBC;
+    delete segmentoCA;
+}
---
+++ tp1/Geometria/Triangulo.h
@@ -0,0 +1,38 @@
+#ifndef TRIANGULO_H
+#define TRIANGULO_H
+
+#include "FiguraGeometrica.h"
+
+/**
+ * Clase que representa un Triangulo, definido por sus 3 vertices.
+ */
+class Triangulo : public FiguraGeometrica
+{
+public:
+    /**
+     * Constructor
+     * PRECONDICION: A,B y C simultaneamente no deben ser puntos colineares
+     * @param verticeA Es uno de los vertices del triangulo
+     * @param verticeB Es uno de los vertices del triangulo
+     * @param verticeC Es uno de los vertices del triangulo
+     */
+    Triangulo(Coordenadas* verticeA, Coordenadas* verticeB, Coordenadas* verticeC);
+
+    virtual ~Triangulo();
+    Coordenadas* getVerticeA() { return verticeA; }
+    Coordenadas* getVerticeB() { return verticeB; }
+    Coordenadas* getVerticeC() { return verticeC; }
+
+    /**
+     * Dibuja el triangulo en la pantalla actual
+     */
+    void dibujar();
+
+protected:
+private:
+    Coordenadas* verticeA;
+    Coordenadas* verticeB;
+    Coordenadas* verticeC;
+};
+
+#endif // TRIANGULO_H
