c054ac46e0a557aa934a835a50fe80f70113d2fb
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-04-15 04:18:41 +0000

    + Se crea clase Letra con su implementacion de dibujo para para 'E' + Se crea codigo en motor::simulacionBre... para mostrar la letra E  (Esto no esta funcionando seguro por alguna boludez pero ya estoy quemado nic, lo subo igual para q lo veas)

---
+++ tp1/Geometria/Letra.h
@@ -0,0 +1,25 @@
+#ifndef LETRA_H
+#define LETRA_H
+
+#include "FiguraGeometrica.h"
+
+
+class Letra : public FiguraGeometrica
+{
+    public:
+        Letra(Coordenadas* centro, char letra);
+
+        Color* getColor() { return this->borde; }
+        void setColor(Color* color);
+        void dibujar();
+        void rellenar();
+        bool contiene(int x, int y);
+
+        virtual ~Letra();
+    protected:
+    private:
+        char letra;
+        Coordenadas* centro;
+};
+
+#endif // LETRA_H
