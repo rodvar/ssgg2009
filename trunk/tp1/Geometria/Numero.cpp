8c816e6e723615c22ade773a95f38e30b283974b
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-04-17 04:36:56 +0000

    + Errores en bresenham implementados 100% con visualizacion de los mismos

--- trunk/tp1/Geometria/Numero.cpp
+++ trunk/tp1/Geometria/Numero.cpp
@@ -1,4 +1,6 @@
 #include "Numero.h"
+#include <stdlib.h>
+#include <sstream>
 
 Numero::Numero(Coordenadas* coordenadas, float numero) {
     this->color = new Color(0,0,0);
@@ -27,109 +29,136 @@ void Numero::setColor(Color* color) {
 }
 
 void Numero::dibujar() {
+    Coordenadas* coordenadas = this->coordenadas->copia();
+	Segmento* linea1 = NULL;
+	Segmento* linea2 = NULL;
+	Segmento* linea3 = NULL;
+	Segmento* linea4 = NULL;
+	Segmento* linea5 = NULL;
+	Segmento* linea6 = NULL;
+	Segmento* linea7 = NULL;
+	Segmento* negativo = new Segmento(new Coordenadas(
+			this->coordenadas->getX()-5,this->coordenadas->getY()+5),
+			new Coordenadas(this->coordenadas->getX()-10,this->coordenadas->getY()+5));
+/*
+	char string[5];
+	sprintf(string,"%f",this->numero);
+*/
 
-	Segmento* linea1 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+0,this->coordenadas->getY()+10),
-			new Coordenadas(this->coordenadas->getX()+5,this->coordenadas->getY()+10));
-	Segmento* linea2 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+0,this->coordenadas->getY()+10),
-			new Coordenadas(this->coordenadas->getX()+0,this->coordenadas->getY()+5));
-	Segmento* linea3 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+5,this->coordenadas->getY()+10),
-			new Coordenadas(this->coordenadas->getX()+5,this->coordenadas->getY()+5));
-	Segmento* linea4 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+0,this->coordenadas->getY()+5),
-			new Coordenadas(this->coordenadas->getX()+5,this->coordenadas->getY()+5));
-	Segmento* linea5 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+0,this->coordenadas->getY()+5),
-			new Coordenadas(this->coordenadas->getX()+0,this->coordenadas->getY()+0));
-	Segmento* linea6 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+5,this->coordenadas->getY()+5),
-			new Coordenadas(this->coordenadas->getX()+5,this->coordenadas->getY()+0));
-	Segmento* linea7 = new Segmento(new Coordenadas(
-			this->coordenadas->getX()+0,this->coordenadas->getY()+0),
-			new Coordenadas(this->coordenadas->getX()+5,this->coordenadas->getY()+0));
-
-	switch ((int)numero) {
-	case 0:
-		linea1->dibujar();
-		linea2->dibujar();
-		linea3->dibujar();
-		linea5->dibujar();
-		linea6->dibujar();
-		linea7->dibujar();
-		break;
-	case 1:
-		linea6->dibujar();
-		linea3->dibujar();
-		break;
-	case 2:
-		linea1->dibujar();
-		linea2->dibujar();
-		linea4->dibujar();
-		linea6->dibujar();
-		linea7->dibujar();
-		break;
-	case 3:
-		linea1->dibujar();
-		linea3->dibujar();
-		linea4->dibujar();
-		linea6->dibujar();
-		linea7->dibujar();
-		break;
-	case 4:
-		linea3->dibujar();
-		linea4->dibujar();
-		linea5->dibujar();
-		linea6->dibujar();
-		break;
-	case 5:
-		linea1->dibujar();
-		linea3->dibujar();
-		linea4->dibujar();
-		linea5->dibujar();
-		linea7->dibujar();
-		break;
-	case 6:
-		linea1->dibujar();
-		linea2->dibujar();
-		linea3->dibujar();
-		linea4->dibujar();
-		linea5->dibujar();
-		linea7->dibujar();
-		break;
-	case 7:
-		linea3->dibujar();
-		linea6->dibujar();
-		linea7->dibujar();
-		break;
-	case 8:
-		linea1->dibujar();
-		linea2->dibujar();
-		linea3->dibujar();
-		linea4->dibujar();
-		linea5->dibujar();
-		linea6->dibujar();
-		linea7->dibujar();
-		break;
-	case 9:
-		linea3->dibujar();
-		linea4->dibujar();
-		linea5->dibujar();
-		linea6->dibujar();
-		linea7->dibujar();
-		break;
-	default: break;
-
+	// Numeros negativos
+	if(this->numero < 0) {
+		negativo->dibujar();
 	}
 
-	delete linea1;
-	delete linea2;
-	delete linea3;
-	delete linea4;
-	delete linea5;
-	delete linea6;
-	delete linea7;
+	string cadena = this->convertirReal(this->numero);
+
+	for(unsigned int i=0;i<cadena.size();i++) {
+		linea1 = new Segmento(new Coordenadas(
+				coordenadas->getX()+0,coordenadas->getY()+10),
+				new Coordenadas(coordenadas->getX()+5,coordenadas->getY()+10));
+		linea2 = new Segmento(new Coordenadas(
+				coordenadas->getX()+0,coordenadas->getY()+10),
+				new Coordenadas(coordenadas->getX()+0,coordenadas->getY()+5));
+		linea3 = new Segmento(new Coordenadas(
+				coordenadas->getX()+5,coordenadas->getY()+10),
+				new Coordenadas(coordenadas->getX()+5,coordenadas->getY()+5));
+		linea4 = new Segmento(new Coordenadas(
+				coordenadas->getX()+0,coordenadas->getY()+5),
+				new Coordenadas(coordenadas->getX()+5,coordenadas->getY()+5));
+		linea5 = new Segmento(new Coordenadas(
+				coordenadas->getX()+0,coordenadas->getY()+5),
+				new Coordenadas(coordenadas->getX()+0,coordenadas->getY()+0));
+		linea6 = new Segmento(new Coordenadas(
+				coordenadas->getX()+5,coordenadas->getY()+5),
+				new Coordenadas(coordenadas->getX()+5,coordenadas->getY()+0));
+		linea7 = new Segmento(new Coordenadas(
+				coordenadas->getX()+0,coordenadas->getY()+0),
+				new Coordenadas(coordenadas->getX()+5,coordenadas->getY()+0));
+
+		switch (cadena[i]) {
+			case '0':
+				linea1->dibujar();
+				linea2->dibujar();
+				linea3->dibujar();
+				linea5->dibujar();
+				linea6->dibujar();
+				linea7->dibujar();
+				break;
+			case '1':
+				linea6->dibujar();
+				linea3->dibujar();
+				break;
+			case '2':
+				linea1->dibujar();
+				linea2->dibujar();
+				linea4->dibujar();
+				linea6->dibujar();
+				linea7->dibujar();
+				break;
+			case '3':
+				linea1->dibujar();
+				linea3->dibujar();
+				linea4->dibujar();
+				linea6->dibujar();
+				linea7->dibujar();
+				break;
+			case '4':
+				linea3->dibujar();
+				linea4->dibujar();
+				linea5->dibujar();
+				linea6->dibujar();
+				break;
+			case '5':
+				linea1->dibujar();
+				linea3->dibujar();
+				linea4->dibujar();
+				linea5->dibujar();
+				linea7->dibujar();
+				break;
+			case '6':
+				linea1->dibujar();
+				linea2->dibujar();
+				linea3->dibujar();
+				linea4->dibujar();
+				linea5->dibujar();
+				linea7->dibujar();
+				break;
+			case '7':
+				linea3->dibujar();
+				linea6->dibujar();
+				linea7->dibujar();
+				break;
+			case '8':
+				linea1->dibujar();
+				linea2->dibujar();
+				linea3->dibujar();
+				linea4->dibujar();
+				linea5->dibujar();
+				linea6->dibujar();
+				linea7->dibujar();
+				break;
+			case '9':
+				linea3->dibujar();
+				linea4->dibujar();
+				linea5->dibujar();
+				linea6->dibujar();
+				linea7->dibujar();
+				break;
+			default: break;
+
+		}
+
+		coordenadas->setX(coordenadas->getX()+7);
+		delete linea1;
+        delete linea2;
+        delete linea3;
+        delete linea4;
+        delete linea5;
+        delete linea6;
+        delete linea7;
+	}
+	delete coordenadas;
+	delete negativo;
 }
 
 void Numero::dibujarPunteado() {
@@ -141,4 +170,11 @@ void Numero::rellenar() {
 
 bool Numero::contiene(int x, int y){
 	return false;
+}
+
+string Numero::convertirReal(float value) {
+    ostringstream o;
+    if (!(o << value))
+        return "";
+    return o.str();
 }
