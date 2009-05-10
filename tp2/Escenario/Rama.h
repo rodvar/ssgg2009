cf9999b4e867c7e11d9705259413b895f79fb9c0
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-05-10 15:23:55 +0000

    + Se agrega orientacion de hojas en rama con get y set + Se agrega dibujo de cilindro en rama + Se prueba la rama en el actualizar de pantalla (test, bug: la rama no toma el color !)

--- tp2/Escenario/Rama.h
+++ tp2/Escenario/Rama.h
@@ -13,10 +13,16 @@ class Rama
     public:
         /**  Constructor
          * @param hoja Es el modelo de hoja para las hojas de la rama
+         * @param orientacion Es la orientacion en grados medidos desde la perpendicular [-45,45]
          **/
-        Rama(Hoja hoja);
+        Rama(Hoja hoja, const short orientacion);
 
         virtual ~Rama();
+        unsigned short int getOrientacionHojas() { return this->orientacionHojas; }
+
+        /** Setea la nueva orientacion verificando que se encuentre en el rango [-45,45]. Si no
+         * lo esta asigna 0**/
+        void setOrientacionHojas(const short int orientacion);
 
         /**
          * Dibuja una rama en el origen de coordenadas cartesianas, con su base en el plano
@@ -27,8 +33,10 @@ class Rama
 
     protected:
     private:
+        GLUquadricObj * qobj;
         Hoja hojas[HOJAS_DEFECTO];
         Color color;
+        short int orientacionHojas;
 };
 
 #endif // RAMA_H
