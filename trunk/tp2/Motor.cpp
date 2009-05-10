63a441edbcb277d52087651054ec6f01c9516474
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-05-10 22:41:57 +0000

    + Se modifica el actualizar() de Pantalla para recibir las listas de datso necesarias para dibujar + Se agregan listas basicas en la implementacion de la actualizacion del motor

--- trunk/tp2/Motor.cpp
+++ trunk/tp2/Motor.cpp
@@ -1,9 +1,33 @@
 #include "Motor.h"
 #include "Visualizacion/Pantalla.h"
+#include "Interaccion/IU.h"
 
 void Motor::actualizar(){
-    Pantalla::getInstancia()->actualizar(this->datos);
-    glutPostRedisplay();
+    list<Dibujable*> escena3d;
+    list<FiguraGeometrica*> sendero2d;
+    list<FiguraGeometrica*> hoja2d;
+
+
+    // TOOO: Aca realiza la simulacion si corresponde, y siempre pasar lo que se tenga q dibujar
+    // al actualizador de Pantalla
+    Pantalla::getInstancia()->actualizar(escena3d, sendero2d, hoja2d);
+    list<Dibujable*>::iterator it3d = escena3d.begin();
+    while (it3d != escena3d.end()){
+        delete *it3d;
+        it3d++;
+    }
+    list<FiguraGeometrica*>::iterator it2dS = sendero2d.begin();
+    while (it2dS != sendero2d.end()){
+        delete *it2dS;
+        it2dS++;
+    }
+    list<FiguraGeometrica*>::iterator it2dI = hoja2d.begin();
+    while (it2dI != hoja2d.end()){
+        delete *it2dI;
+        it2dI++;
+    }
+
+
 }
 
 void Motor::limpiarBufferDatos(){
