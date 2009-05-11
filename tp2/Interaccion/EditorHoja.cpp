886cec1f94fc8beb4e938eadb7aee08dd148be00
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-05-11 01:46:34 +0000

    + Se corrige bug en Pantalla: mal uso de glViewport + Se corrige posicion de editores en su creacion

--- tp2/Interaccion/EditorHoja.cpp
+++ tp2/Interaccion/EditorHoja.cpp
@@ -2,7 +2,7 @@
 
 EditorHoja::EditorHoja(float ancho, float alto):Editor(ancho,alto)
 {
-    Coordenadas coordenadas(ancho*0.70f + this->marco.getBase()/2,alto*0.70f + this->marco.getAltura()/2);
+    Coordenadas coordenadas(ancho*0.70f + this->marco.getBase()/2,alto*0.50f - this->marco.getAltura()/2);
     this->marco.setCentro(coordenadas);
 }
 
