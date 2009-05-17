b4fcfd4e38cccc6766ab36f20624bcf44c403901
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-05-17 23:19:53 +0000

    // COMMIT PARA PODER TRABAJAR DESDE OTRA PC + Intentando lograr la recursion de dibujo del arbol
    * se agrega clase Arbol
    * se agrega doc de opengl

---
+++ trunk/tp2/Escenario/Arbol.h
@@ -0,0 +1,34 @@
+#ifndef ARBOL_H_INCLUDED
+#define ARBOL_H_INCLUDED
+
+#include "../Geometria/Curva.h"
+#include "Rama.h"
+
+class Arbol : public Dibujable{
+    public:
+        /**  Constructor: Construye una rama de orientacion aleatoria con hojas de orientacion aleatoria
+         * @param modeladoHoja Es el modelo de hoja para las hojas de la rama
+         * @param niveles Cantidad de ramas del arbol
+         **/
+        Arbol(Curva* modeladoHoja, const unsigned short int niveles);
+
+        virtual ~Arbol();
+
+        /**
+         * Dibuja el arbol en forma recursiva
+         */
+        void dibujar();
+
+    private:
+        Rama* raiz;
+        unsigned short int niveles;
+
+        // crea el arbol
+        Rama* crearArbol(Rama* raiz);
+
+        //Dibuja el arbol el forma recursiva a partir de la raiz pasada por parametro
+        void dibujarRecursivo(Rama* raiz);
+
+};
+
+#endif // ARBOL_H_INCLUDED
