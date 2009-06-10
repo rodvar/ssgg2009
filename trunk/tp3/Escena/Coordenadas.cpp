b6886a41e4520e00a8079b1e35bd94e101116b6f
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-06-10 03:03:14 +0000

    + Se crea base para el proyecto TP3    - Basado en el framework 3D provisto por la catedra    - Basado en la filosofia "Small is Beautiful"
    aganarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrRRRRRRRRRRRRRRRRRRRRRR

---
+++ trunk/tp3/Escena/Coordenadas.cpp
@@ -0,0 +1,44 @@
+#include "Coordenadas.h"
+
+Coordenadas::Coordenadas() { this->x = 0; this->y = 0; this->z = 0; }
+
+Coordenadas::Coordenadas(int x, int y) { this->x = x; this->y = y; this->z = 0; }
+Coordenadas::Coordenadas(int x, int y, int z) { this->x = x; this->y = y; this->z = z; }
+
+Coordenadas::Coordenadas(float x, float y) { this->x = x; this->y = y; this->z = 0; }
+Coordenadas::Coordenadas(float x, float y, float z) { this->x = x; this->y = y; this->z = z; }
+
+Coordenadas::Coordenadas(double x, double y) { this->x = x; this->y = y; this->z = 0; }
+Coordenadas::Coordenadas(double x, double y, double z) { this->x = x; this->y = y; this->z = z; }
+
+
+Coordenadas::~Coordenadas()
+{
+    //dtor
+}
+
+Coordenadas::Coordenadas(const Coordenadas& coordenadas){
+    this->x = coordenadas.x;
+    this->y = coordenadas.y;
+    this->z = coordenadas.z;
+}
+
+bool Coordenadas::tieneMayorX(Coordenadas* c){
+    bool mayor;
+    (this->x >= c->getX())? mayor = true : mayor = false;
+    return mayor;
+}
+
+double Coordenadas::distancia (Coordenadas hasta){
+	float deltaX = hasta.getX() - this->x;
+	float deltaY = hasta.getY() - this->y;
+    return sqrt(deltaX*deltaX + deltaY*deltaY);
+}
+
+bool Coordenadas::operator == (Coordenadas coordenadas){
+    return ((this->x == coordenadas.getX()) && (this->y == coordenadas.getY()));
+}
+
+void Coordenadas::dibujar(){
+    glVertex3f(this->x,this->y,this->z);
+}
---
+++ trunk/tp3/Visualizacion/OpenGLHelper.h
@@ -0,0 +1,59 @@
+#ifndef OPENGLHELPER_H
+#define OPENGLHELPER_H
+
+#include <list>
+#include <string>
+#include "GL/glut.h"
+#include "../Escena/Coordenadas.h"
+
+#define GL_PI 3.14159265359
+
+/**
+ * @brief Esta clase funciona como wrapper de la libreria grafica OpenGL. Su funcionalidad
+ * es bridar 'primitivas' sencillas que permitan modelar dibujos complejos.
+ */
+class OpenGLHelper
+{
+    public:
+        OpenGLHelper();
+        ~OpenGLHelper();
+
+        /**
+         * @brief Dibuja una recta desde el punto (0,0,0) hasta el punto (0,0,longitud)
+         */
+        static void dibujarRecta(const float longitud);
+
+        /**
+         * @brief Dibuja la curva uniendo sus puntos discretos con GL_LINE_STRIP
+         */
+        static void dibujarCurva(std::list<Coordenadas> discretizacion);
+
+        /**
+         * @brief Dibuja una circunferencia en el origen con el radio especificado
+         * @param precision Es el paso de discretizacion medido en radianes (menor a 2PI)
+         */
+        static void dibujarCircunferencia(const float radio, const float precision);
+
+        /**
+         * Dibuja los ejes cartesianos x,y,z
+         */
+        static void dibujarEjes();
+
+        /**
+         * @brief Dibuja una grilla XY en el plano z=0
+         */
+        static void dibujarGrillaXY();
+
+        /**
+         * @brief Muestra si hubo algun error en el pipeline grafico por pantalla
+         * @return true si hubo error, false en caso contrario
+         */
+        static bool mostrarError();
+
+
+
+    protected:
+    private:
+};
+
+#endif // OPENGLHELPER_H
