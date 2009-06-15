4cf3040246d3a2a2a5c179ab4cca1978bc0d51c9
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-06-15 20:18:22 +0000

    + Se agrega funcionalidad al OpenGLHelper para dibujar otros elementos basicos como cuadrados , cilindros, y un "Samba" basico para algunos elementos del modelo del tp. + Se agrega clase PintorCurva cuya funcionalidad es poder dibujar currvas de cualquier tipo, incluso pedazos de superficies o "gajos"

--- trunk/tp3/Visualizacion/OpenGLHelper.cpp
+++ trunk/tp3/Visualizacion/OpenGLHelper.cpp
@@ -19,6 +19,19 @@ void OpenGLHelper::dibujarRecta(float longitud){
     glEnd();
 }
 
+void OpenGLHelper::dibujarRectangulo(const float base, const float altura){
+    glBegin(GL_QUADS);
+        glVertex3f(0.0f,0.0f,0.0f);
+        glVertex3f(base,0.0f,0.0f);
+        glVertex3f(base,altura,0.0f);
+        glVertex3f(0.0f,altura,0.0f);
+    glEnd();
+}
+
+void OpenGLHelper::dibujarCuadrado(const float lado){
+    dibujarRectangulo(lado,lado);
+}
+
 void OpenGLHelper::dibujarCurva(std::list<Coordenadas> discretizacion){
     std::list<Coordenadas>::iterator it = discretizacion.begin();
     glBegin(GL_LINE_STRIP);
@@ -29,54 +42,76 @@ void OpenGLHelper::dibujarCurva(std::list<Coordenadas> discretizacion){
     glEnd();
 }
 
-void OpenGLHelper::dibujarCircunferencia(const float radio, const float paso){
+void OpenGLHelper::dibujarCircunferencia(const float paso){
+    float radio = 1.0f;
     float x,y;
     glBegin(GL_LINE_STRIP);
-    for(float alfa = 0.0f; alfa < (2 * GL_PI); alfa += paso){
-        x = radio * cos(alfa);
-        y = radio * sin(alfa);
-        glVertex3f(x,y,0.0f);
+        for(float alfa = 0.0f; alfa < DOSPI; alfa += paso){
+            x = radio * cos(alfa);
+            y = radio * sin(alfa);
+            glVertex3f(x,y,0.0f);
+        }
+    glEnd();
+}
+
+void OpenGLHelper::dibujarCirculo(const float paso){
+    float radio = 1.0f;
+    float x,y;
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex3f(0.0f,0.0f,0.0f);
+        for(float alfa = 0.0f; alfa < DOSPI; alfa += paso){
+            x = radio * cos(alfa);
+            y = radio * sin(alfa);
+            glVertex3f(x,y,0.0f);
+        }
+        glVertex3f(radio,0.0f,0.0f);
+    glEnd();
+}
+
+void OpenGLHelper::dibujarCilindro(const float precision){
+    GLUquadricObj* quadric = gluNewQuadric();
+    gluCylinder(quadric, UNITARIO, UNITARIO, UNITARIO , precision, precision);
+    gluDeleteQuadric(quadric);
+}
+
+void OpenGLHelper::dibujarAbanico(const float paso){
+    const float radio = UNITARIO;
+    unsigned short int tamano = (int)(DOSPI/paso + 1);
+    Coordenadas puntos[tamano];
+    Matematica::discretizarCircunferencia(puntos, radio, paso, NULO);
+
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex3f(0.0f,0.0f,0.0f);
+        for (int j =0; j < tamano; j++)
+            glVertex3f(puntos[j].getX(), puntos[j].getY(), 0.0f);
+        glVertex3f(puntos[0].getX(), puntos[0].getY(), 0.0f);
+    glEnd();
+}
+
+void OpenGLHelper::dibujarSamba(const float precision){
+    float deltaAlfa = 360.0f/precision;
+    float base = sin(DOSPI/precision);
+    float radio = 1.0f;
+    float altura = 1.0f;
+    Coordenadas normal1 = Matematica::calcularNormal(Coordenadas(1,0,0),-deltaAlfa);
+    Coordenadas normal2 = Matematica::calcularNormal(Coordenadas(1,0,0),deltaAlfa);
+
+    for (int i = 0; i < precision ; i++){
+        glPushMatrix();
+            glRotatef(i*deltaAlfa,0,0,1);
+            glBegin(GL_QUADS);// Baranda
+                glNormal3f(normal1.getX(),normal1.getY(),normal1.getZ()); // Asegura sup suave
+                glVertex3f(radio,-base/2,0.0f);
+                glVertex3f(radio,-base/2,altura);
+                glNormal3f(normal2.getX(),normal2.getY(),normal2.getZ()); // Asegura sup suave
+                glVertex3f(radio,base/2,altura);
+                glVertex3f(radio,base/2,0.0f);
+            glEnd();
+        glPopMatrix();
     }
-    glEnd();
-}
-
-void OpenGLHelper::dibujarAbanico(const float radio, const float paso){
-    unsigned short int tamano = (int)((2 * GL_PI)/paso + 1);
-    Coordenadas puntos[tamano];
-    discretizarCurva(puntos, radio, paso);
-
-    glBegin(GL_LINE_STRIP);
-        for (int j =0; j < tamano; j++){
-            glVertex3f(0.0f, 0.0f, 0.0f);
-            glVertex3f(puntos[j].getX(), puntos[j].getY(), 0.0f);
-
-            glVertex3f(puntos[j].getX(), puntos[j].getY(), 0.0f);
-            if (j < tamano-1)
-                glVertex3f(puntos[j+1].getX(), puntos[j+1].getY(), 0.0f);
-            else
-                glVertex3f(puntos[0].getX(), puntos[0].getY(), 0.0f);
-        }
-    glEnd();
-}
-
-void OpenGLHelper::dibujarBarandal(const float radio, const float paso, const float alto){
-    // TODO: ESTO VA EN EL FARO!!! Atomizar operacion basica para lograrlo
-    unsigned short int tamano = (int)((2 * GL_PI)/paso + 1);
-    Coordenadas puntos[tamano];
-    discretizarCurva(puntos, radio, paso);
-
-    glBegin(GL_LINE_STRIP);
-        for (int j =0; j < tamano; j++){
-            glVertex3f(0.0f, 0.0f, 0.0f);
-            glVertex3f(puntos[j].getX(), puntos[j].getY(), 0.0f);
-
-            glVertex3f(puntos[j].getX(), puntos[j].getY(), 0.0f);
-            if (j < tamano-1)
-                glVertex3f(puntos[j+1].getX(), puntos[j+1].getY(), 0.0f);
-            else
-                glVertex3f(puntos[0].getX(), puntos[0].getY(), 0.0f);
-        }
-    glEnd();
+    glPushMatrix();
+        OpenGLHelper::dibujarAbanico(0.20f*radio);
+    glPopMatrix();
 }
 
 void OpenGLHelper::dibujarEjes()
@@ -124,15 +159,4 @@ bool OpenGLHelper::mostrarError(){
     if (error != GL_NO_ERROR)
         std::cout << gluErrorString(error) << std::endl;
     return (error != GL_NO_ERROR);
-}
-
-// private
-void OpenGLHelper::discretizarCurva(Coordenadas* vector, const float radio, const float paso){
-    Coordenadas punto;
-    unsigned short int i = 0;
-    for(float alfa = 0.0f; alfa < (2 * GL_PI); alfa += paso){
-        punto.setX((float)(radio * cos(alfa)));
-        punto.setY((float)(radio * sin(alfa)));
-        vector[i++] = punto;
-    }
 }
---
+++ trunk/tp3/Visualizacion/PintorCurva.cpp
@@ -0,0 +1,81 @@
+#include "PintorCurva.h"
+#include "../Geometria/Matematica.h"
+
+PintorCurva::PintorCurva()
+{
+}
+
+PintorCurva::~PintorCurva()
+{
+}
+
+void PintorCurva::pintar(std::vector<Coordenadas> &curvePoints)
+{
+    /* Se unen los puntos calculados con segmentos de recta. */
+    glBegin(GL_LINE_STRIP);
+        for(unsigned int i=0; i<curvePoints.size(); i++)
+            glVertex3f(curvePoints[i].getX(), curvePoints[i].getY(), curvePoints[i].getZ());
+    glEnd();
+}
+
+void PintorCurva::pintarGajo(std::vector<Coordenadas> &puntosA, std::vector<Coordenadas> &puntosB){
+    Coordenadas normal1;
+    Coordenadas normal2;
+    Coordenadas suma;
+    double angulo;
+    if (puntosA.size() == puntosB.size()){
+        glBegin(GL_TRIANGLE_STRIP);
+            for(unsigned int i=0; i<puntosA.size(); i++){
+                suma = puntosA[i] + puntosB[i];
+                angulo = puntosA[i].angulo(puntosB[i]);
+                normal1 = Matematica::calcularNormal(suma,-angulo);
+                normal2 = Matematica::calcularNormal(suma,angulo);
+                glNormal3f(normal1.getX(),normal1.getY(),normal1.getZ());
+                glVertex3f(puntosA[i].getX(), puntosA[i].getY(), puntosA[i].getZ());
+                glNormal3f(normal2.getX(),normal2.getY(),normal2.getZ());
+                glVertex3f(puntosB[i].getX(), puntosB[i].getY(), puntosB[i].getZ());
+            }
+        glEnd();
+    }
+}
+
+void PintorCurva::pintarPuntosControl(std::vector<Coordenadas>& cPoints, double radius, bool bezierCP)
+{
+	/* Se unen los puntos de control mediante una linea punteada */
+	glLineStipple(2,0xAAAA) ;
+	glEnable(GL_LINE_STIPPLE) ;
+		glBegin(GL_LINE_STRIP) ;
+			for(unsigned int i=0 ; i<cPoints.size() ; ++i) {
+			    glVertex3f(cPoints[i].getX(), cPoints[i].getY(), cPoints[i].getZ());
+			}
+		glEnd() ;
+	glDisable(GL_LINE_STIPPLE) ;
+
+	/* se muestran los puntos de control mediante círculos */
+	for(unsigned int i=0 ; i<cPoints.size() ; ++i) {
+		this->dibujarCirculoRelleno(cPoints[i].getX(),cPoints[i].getY(),radius) ;
+	}
+}
+
+void PintorCurva::dibujarCirculoRelleno(double x,double y,double radius)
+{
+	const double degInRadian = 57.29577957795135 ;
+
+	glEnable(GL_BLEND);
+	double y1=y;
+	double x1=x;
+	glBegin(GL_TRIANGLES);
+	for(int i=0;i<=360;i++)
+	{
+		double angle=(float)(((double)i)/degInRadian);
+		double x2=x+(radius*(float)sin((double)angle));
+		double y2=y+(radius*(float)cos((double)angle));
+		glVertex2d(x,y);
+		glVertex2d(x1,y1);
+		glVertex2d(x2,y2);
+		y1=y2;
+		x1=x2;
+	}
+	glEnd();
+	glDisable(GL_BLEND);
+}
