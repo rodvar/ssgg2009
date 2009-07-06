9df1e2dc9d668b1f8cd5ff101000a44d6f53d04c
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-07-06 23:54:21 +0000

    + Se crea la clase Camara, encapsulando todo lo referido a esta de OpenGL. Se utiliza una unica Camara como variable global del sistema.

---
+++ trunk/tp4/Visualizacion/Camara.cpp
@@ -0,0 +1,39 @@
+#include "Camara.h"
+#include <GL/glut.h>
+
+Camara::Camara()
+{
+    this->eye[0] = 15.0;
+    this->eye[1] = 15.0;
+    this->eye[2] = 5.0;
+    this->at[0] = 0.0f;
+    this->at[1] = 0.0f;
+    this->at[2] = 0.0f;
+    this->up[0] = 0.0f;
+    this->up[1] = 0.0f;
+    this->up[2] = 1.0f;
+    this->zoom = 45.0;
+    this->anguloXY = 0.0;
+    this->anguloZ = 0.0;
+}
+
+void Camara::incrementarRotacionXY(const float incremento){
+    this->anguloXY += incremento;
+}
+void Camara::incrementarRotacionZ(const float incremento){
+    this->anguloZ += incremento;
+}
+void Camara::incrementarZoom(const float incremento){
+    this->zoom += incremento;
+}
+void Camara::incrementarZVer(const float incremento){
+    this->at[2] += incremento;
+}
+
+void Camara::preparar(){
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt (eye[0], eye[1], eye[2], at[0], at[1], at[2], up[0], up[1], up[2]);
+	glRotatef(anguloZ,-1,1,0);
+	glRotatef(anguloXY,0,0,1);
+}
---
+++ trunk/tp4/Visualizacion/Camara.h
@@ -0,0 +1,42 @@
+#ifndef CAMARA_H
+#define CAMARA_H
+
+/**
+ * Representa la camara con la que se enfoca la escena.
+ */
+class Camara
+{
+    public:
+        /*Constructor. Deja la camara lista para tomar imagenes en modo standart*/
+        Camara();
+
+        /*getters*/
+        const float* getOjo(){ return this->eye;} // Posicion del ojo de la camara
+        const float* getArriba(){ return this->up;} // Hacia donde es el arriba de la camara?
+        const float* getVer(){ return this->at;} // Posicion de lo que mira
+        const float getZoom(){ return this->zoom;}
+        const float getAnguloXY(){ return this->anguloXY;} // Rotacion XY
+        const float getAnguloZ(){ return this->anguloZ;} // Rotacion Z
+
+        /*setters*/
+        void incrementarRotacionXY(const float incremento);
+        void incrementarRotacionZ(const float incremento);
+        void incrementarZoom(const float incremento);
+        void incrementarZVer(const float incremento);
+
+        /**
+         * Configura la camara para tomar imagenes de acuerdo a los parametros actuales
+         */
+        void preparar();
+
+    private:
+        // Variables que controlan la ubicaci�n de la c�mara en la Escena 3D
+        float eye[3];
+        float at[3];
+        float up[3];
+        float zoom;
+        float anguloXY;
+        float anguloZ;
+};
+
+#endif // CAMARA_H
