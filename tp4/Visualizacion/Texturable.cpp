880c72dcdce7486ca916e0012a2c8bb5aada60dd
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-07-09 16:47:04 +0000

    + Se crea clase abstracta Texturable que encapsula todo lo  neceasrio para que un objeto se pueeda dibujar texturado. El metodo abstracto dibujar debera incluir para los hijos de esta clase, las coordenadas de textels a mapear, y preguntar si el objeto tiene la textura seteada antes de ejecutar esos mapeos (sino va a texturar con la ultima textura que se haya configurado en el pipeline)
    - Nico te deje esto como plantilla para q metas lo de openGL ;)

---
+++ tp4/Visualizacion/Texturable.cpp
@@ -0,0 +1,27 @@
+#include "Texturable.h"
+
+Texturable::Texturable()
+{
+   //TODO : si algo se ejecuta siempre independientemente del nombre del archivo iria aca
+}
+
+Texturable::~Texturable()
+{
+    //dtor
+}
+
+void Texturable::setTextura(std::string nombre) {
+    this->nombreArchivo = nombre;
+}
+
+bool Texturable::listoTexturar(){
+    return nombreArchivo != ""; // NIC FIJATE SI ESTO ANDA!
+}
+
+void Texturable::dibujarTexturado(){
+    // TODO: Nico aca configuras el Pipeline para que pueda dibujar texturado, supongo q tenes
+    // que abrir el archivo de (this->nombreArchivo) y demas
+
+    this->dibujar();
+}
+
