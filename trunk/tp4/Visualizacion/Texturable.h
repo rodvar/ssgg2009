396818cd4de975b4f27029a7ab5bdc7b1ed5f376
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-07-09 16:47:04 +0000

    + Se crea clase abstracta Texturable que encapsula todo lo  neceasrio para que un objeto se pueeda dibujar texturado. El metodo abstracto dibujar debera incluir para los hijos de esta clase, las coordenadas de textels a mapear, y preguntar si el objeto tiene la textura seteada antes de ejecutar esos mapeos (sino va a texturar con la ultima textura que se haya configurado en el pipeline)
    - Nico te deje esto como plantilla para q metas lo de openGL ;)

---
+++ trunk/tp4/Visualizacion/Texturable.h
@@ -0,0 +1,42 @@
+#ifndef TEXTURABLE_H
+#define TEXTURABLE_H
+
+#include "Dibujable.h"
+#include <string>
+
+/**
+ *   Clase
+ */
+class Texturable : public Dibujable
+{
+    public:
+        // Constructor / Destructor
+        Texturable();
+        virtual ~Texturable();
+
+        /** Set de textura **/
+        void setTextura(std::string nombre);
+
+        /**
+         * @brief Prepara todo lo necesario para que se pueda dibujar el objeto con su
+         *        Textura aplicada. Luego llama a this->dibujar() el cual debera indicar
+         *        el mapeo de la textura con coordenadas de Texels
+         */
+        void dibujarTexturado();
+
+        /** Dibujar el elemento dibujable **/
+        virtual void dibujar() = 0;
+
+    protected:
+        /**
+         * @brief Devuelve true si el archivo se puede texturar, false en caso contrario.
+         *      Las clases hijas deberian preguntar esto antes de ejecutar comandos de
+         *      Coordenadas de Texels
+         */
+        bool listoTexturar();
+
+        // Nombre del archivo donde esta la textura a apĺicar
+        std::string nombreArchivo; // Si es "" significa que no se seteo la textura
+};
+
+#endif // TEXTURABLE_H
