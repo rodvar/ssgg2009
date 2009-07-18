02766ca10577cc16d39a3d838f3164c0d8b0b209
nicohen <nicohen@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-07-18 00:00:23 +0000

    + Se agrega imagen para mostrar el cielo texturado

--- tp4/Escena/Domo.cpp
+++ tp4/Escena/Domo.cpp
@@ -10,7 +10,7 @@ Domo::~Domo() {}
 void Domo::dibujar() {
     // TODO: Agregar coordenadas de la textura glTexCoord2f(x,y) para cada vertice
 
-    glBindTexture(GL_TEXTURE_2D, this->idTextura);
+	glBindTexture(GL_TEXTURE_2D, this->idTextura);
 	//Bottom
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -21,15 +21,19 @@ void Domo::dibujar() {
 			glBegin(GL_QUADS); // Nico esto es el piso ?!?
 				c=Matematica::obtenerNormalizado(Coordenadas(UNITARIO,UNITARIO,-UNITARIO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(1)
+				glTexCoord2f(0.5,0.0);
 				glVertex3f(NULO,NULO,NULO);
 				c=Matematica::obtenerNormalizado(Coordenadas(-UNITARIO,UNITARIO,-UNITARIO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(2)
+				glTexCoord2f(0.66,0.0);
 				glVertex3f(lado,NULO,NULO);
 				c=Matematica::obtenerNormalizado(Coordenadas(-UNITARIO,-UNITARIO,-UNITARIO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(3)
+				glTexCoord2f(0.66,1.0);
 				glVertex3f(lado,lado,NULO);
 				c=Matematica::obtenerNormalizado(Coordenadas(UNITARIO,-UNITARIO,-UNITARIO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(4)
+				glTexCoord2f(0.5,1.0);
 				glVertex3f(NULO,lado,NULO);
 			glEnd();
 		glPopMatrix();
@@ -38,15 +42,19 @@ void Domo::dibujar() {
 			glBegin(GL_QUADS);
 				c=Matematica::obtenerNormalizado(Coordenadas(UNITARIO,UNITARIO,-UNITARIO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(1)
+				glTexCoord2f(0.0,0.5);
 				glVertex3f(NULO,NULO,NULO);
 				c=Matematica::obtenerNormalizado(Coordenadas(UNITARIO,UNITARIO,NULO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(5)
+				glTexCoord2f(0.16,0.5);
 				glVertex3f(lado,NULO,NULO);
 				c=Matematica::obtenerNormalizado(Coordenadas(UNITARIO,-UNITARIO,NULO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(6)
+				glTexCoord2f(0.16,1.0);
 				glVertex3f(lado,lado,NULO);
 				c=Matematica::obtenerNormalizado(Coordenadas(UNITARIO,-UNITARIO,-UNITARIO));
 				glNormal3f(c.getX(),c.getY(),c.getZ()); //(4)
+				glTexCoord2f(0.0,1.0);
 				glVertex3f(NULO,lado,NULO);
 			glEnd();
 		glPopMatrix();
