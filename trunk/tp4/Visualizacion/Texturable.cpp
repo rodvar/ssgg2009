c4e84f9dd0db7e3c2b691d2a299b89f7c86af12c
nicohen <nicohen@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-07-18 21:40:19 +0000

    + Domo texturado, faltan corregir un poco el tema de las normales

--- trunk/tp4/Visualizacion/Texturable.cpp
+++ trunk/tp4/Visualizacion/Texturable.cpp
@@ -39,8 +39,8 @@ void Texturable::cargarImagen(){
         }
     // TODO: Nico busca el metodo glTexEnvf a ver como combiene configurarlo segun el caso
     // por un tema de performance..
-        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
         glEnable(GL_TEXTURE_2D);
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
         glBindTexture(GL_TEXTURE_2D,this->idTextura);
     }
     else{
