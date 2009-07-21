41a99082a5ea40fc1a36285fae7a95d7481399a4
rodvar <rodvar@6da81292-15a5-11de-a4db-e31d5fa7c4f0>
2009-07-21 22:33:32 +0000

    + Se corrigen defectos de camara     1) limitacion al zoom in/out     2) limitaciones de giro para cada camara     3) movimiento de camara en vista desde lejos sobre el mar

--- tp4/Visualizacion/Camara.cpp
+++ tp4/Visualizacion/Camara.cpp
@@ -5,18 +5,31 @@
 Camara::Camara()
 {
     this->setVistaStd();
-    this->zoom = 45.0;
     this->rotacionDesdeOjo = false;
+    this->reset();
+    this->rangoZ[0] = -13.0f;
+    this->rangoZ[1] = 180.0f - 13.0f;
 }
 
 void Camara::incrementarRotacionXY(const float incremento){
     this->anguloXY += incremento;
+    if (abs(this->anguloXY) == 360)
+        this->anguloXY = 0;
+
 }
 void Camara::incrementarRotacionZ(const float incremento){
     this->anguloZ += incremento;
+    if (this->anguloZ < this->rangoZ[0])
+        this->anguloZ = this->rangoZ[0];
+    else if (this->anguloZ >this->rangoZ[1])
+        this->anguloZ = this->rangoZ[1];
 }
 void Camara::incrementarZoom(const float incremento){
     this->zoom += incremento;
+    if (this->zoom < MAX_ZOOM) // el valor de zoom menor es maximo
+        this->zoom = MAX_ZOOM;
+    else if (this->zoom > MIN_ZOOM) // y viceversa
+        this->zoom = MIN_ZOOM;
 }
 void Camara::incrementarZVer(const float incremento){
     this->at[2] += incremento;
@@ -39,6 +52,7 @@ void Camara::switchVista(){
             vista = 0;
             break;
     }
+    this->reset();
 }
 
 void Camara::preparar(){
@@ -46,14 +60,8 @@ void Camara::preparar(){
 	glLoadIdentity();
 	gluLookAt (eye[0], eye[1], eye[2], at[0], at[1], at[2], up[0], up[1], up[2]);
 
-    /*
-    glRotated(roll, 0.0, 0.0, 1.0);
-      glRotated(pitch, 0.0, 1.0, 0.0);  probar esto
-      glRotated(heading, 1.0, 0.0, 0.0);
-    */
 	if (this->rotacionDesdeOjo)
         glTranslated(eye[0], eye[1], eye[2]);
-    // TODO: Calcular el ejeRotacion de acuerdo a la posicion del ojo!!! :)
 	glRotatef(anguloZ,ejeRotacionZ[0],ejeRotacionZ[1],ejeRotacionZ[2]);
 	glRotatef(anguloXY,ejeRotacionXY[0],ejeRotacionXY[1],ejeRotacionXY[2]);
 	if (this->rotacionDesdeOjo)
@@ -79,6 +87,8 @@ void Camara::setVistaStd(){
     this->anguloXY = 0.0;
     this->anguloZ = 0.0;
     this->rotacionDesdeOjo = false;
+    this->rangoZ[0] = -13.0f;
+    this->rangoZ[1] = 180.0f - 13.0f;
 }
 
 void Camara::setVistaBalconFaro(){
@@ -102,6 +112,8 @@ void Camara::setVistaBalconFaro(){
     this->anguloXY = 0.0;
     this->anguloZ = 0.0;
     this->rotacionDesdeOjo = true;
+    this->rangoZ[0] = -90.0f;
+    this->rangoZ[1] = 30.0f;
 }
 
 void Camara::setVistaBaseIsla(){
@@ -124,6 +136,8 @@ void Camara::setVistaBaseIsla(){
     this->anguloXY = 0.0;
     this->anguloZ = 0.0;
     this->rotacionDesdeOjo = true;
+    this->rangoZ[0] = -90.0f;
+    this->rangoZ[1] = 90.0f;
 }
 
 void Camara::setVistaAguaLejos(){
@@ -139,10 +153,18 @@ void Camara::setVistaAguaLejos(){
     this->ejeRotacionXY[0] = 0;
     this->ejeRotacionXY[1] = 0;
     this->ejeRotacionXY[2] = 1;
-    this->ejeRotacionZ[0] = -1;
+    this->ejeRotacionZ[0] = 0;
     this->ejeRotacionZ[1] = 1;
     this->ejeRotacionZ[2] = 0;
     this->anguloXY = 0.0;
     this->anguloZ = 0.0;
     this->rotacionDesdeOjo = false;
+    this->rangoZ[0] = 0.0f;
+    this->rangoZ[1] = 179.0f;
+}
+
+void Camara::reset(){
+    this->anguloZ = 0.0f;
+    this->anguloXY = 0.0f;
+    this->zoom = ZOOM_STD;
 }
